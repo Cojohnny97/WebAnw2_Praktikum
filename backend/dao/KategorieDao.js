df95c975eb3b8d46023816f296511c3aeece97ab
Cojohnny97 <81743238+Cojohnny97@users.noreply.github.com>
2021-12-05 21:17:00 +0100

    finished kategorieDao

---
+++ backend/dao/KategorieDao.js
@@ -0,0 +1,85 @@
+const helper = require('../helper.js');
+
+class KategorieDao {
+
+    constructor(connection) {
+        this._conn = connection
+    }
+
+    getById(id) {
+        var sql = 'SELECT * FROM Kategorie WHERE ID=?'
+        var statement = this._conn.prepare(sql)
+        var result = statement.get(id)
+
+        if (helper.isUndefined(result)) {
+            throw new Error('No Record found by id=' + id)
+        }
+        result = helper.objectKeysToLower(result)
+        return result
+    }
+
+    getAll () {
+        var sql = 'SELECT * from Kategorie'
+        var statement = this._conn.prepare(sql)
+        var result = statement.all()
+
+        if (helper.isArrayEmpty(result)) {
+            return []
+        }
+
+        return helper.arrayObjectKeysToLower(result)
+    }
+
+    exists(id) {
+        var sql = 'SELECT COUNT(ID) AS cnt FROM Kategorie WHERE ID=?'
+        var statement = this._conn.prepare(sql)
+        var result = statement.get(id)
+
+        return result.cnt == 1 ? true : false
+    }
+
+    create(name = '', bildpfad = '') {
+        var sql = 'INSERT INTO Kategorie (Name,Bildpfad) VALUES (?,?)'
+        var statement = this._conn.prepare(sql)
+        var params = [name, bildpfad]
+        var result = statement.run(params)
+
+        if (result.changes != 1) {
+            throw new Error('Could not insert new Record. Data: ' + params)
+        }
+        return this.getById(result.lastInsertRowid)
+    }
+
+    update(id, name = '', bildpfad = '') {
+        var sql = 'UPDATE Kategorie SET Name=?,Bildpfad=? WHERE ID=?'
+        var statement = this._conn.prepare(sql)
+        var params = [name, bildpfad, id]
+        var result = statement.run(params)
+
+        if (result.changes != 1) {
+            throw new Error('Could not update existing Record. Data: ' + params)
+        }
+        return this.loadById(id)
+    }
+
+    delete(id) {
+        try {
+            var sql = 'DELETE FROM Kategorie WHERE ID=?'
+            var statement = this._conn.prepare(sql)
+            var result = statement.run(id)
+
+            if (result.changes != 1) {
+                throw new Error('Could not delete Record by id=' + id)
+            }
+            return true
+        } catch (ex) {
+            throw new Error('Could not delete Record by id=' + id + '. Reason: ' + ex.message)
+        }
+    }
+
+    toString() {
+        helper.log('KategorieDao [_conn=' + this._conn + ']')
+    }
+}
+
+module.exports = KategorieDao
